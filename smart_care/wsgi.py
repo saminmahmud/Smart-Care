47c89e601e5e09a89d217915fd133a9b7190264c
saminmahmud <samin.mahmud18@gmail.com>
2026-05-10 10:29:09 +0600

    setup vercel deployment

--- smart_care/wsgi.py
+++ smart_care/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_care.settings')
 
 application = get_wsgi_application()
+
+app = application  # For Vercel
