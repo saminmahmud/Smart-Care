56f1077647a5d44e5f45f400de3b51ca25aea1e0
saminmahmud <samin.mahmud18@gmail.com>
2026-05-10 12:48:17 +0600

    add email settings

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -175,3 +175,11 @@ ServerSecret=env("ServerSecret")
 
 # SSLCOMMERZ
 STRIPE_SECRET_KEY=env("STRIPE_SECRET_KEY")
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
